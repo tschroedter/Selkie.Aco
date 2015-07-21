1dbb3f8f265d635eaf32be834c68fb07f596fbef
Thomas Schroedter <Thomas.Schroedter@gmx.net>
2015-07-22 09:17:33 +1000

    Updated from private repository

---
+++ Selkie.Services.Aco.Tests/Handlers/XUnit/StartHandlerTests.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using JetBrains.Annotations;
+using NSubstitute;
+using Ploeh.AutoFixture.Xunit;
+using Selkie.Services.Aco.Common.Messages;
+using Selkie.Services.Aco.Handlers;
+using Selkie.XUnit.Extensions;
+using Xunit.Extensions;
+
+namespace Selkie.Services.Aco.Tests.Handlers.XUnit
+{
+    [ExcludeFromCodeCoverage]
+    //ncrunch: no coverage start
+    public class StartHandlerTests
+    {
+        [Theory]
+        [AutoNSubstituteData]
+        public void HandleCallsStopTest([NotNull] [Frozen] IServiceColony colony,
+                                        [NotNull] StartHandler sut)
+        {
+            // Arrange
+            // Act
+            sut.Handle(new StartMessage
+                       {
+                           Times = 1000
+                       });
+
+            // Assert
+            colony.Received().Start(1000);
+        }
+    }
+}
